9a27680f616c789eb79801903a35ef9819d3657c
dylanEstiam <dylan.regnault@estiam.com>
2021-04-14 15:24:16 +0200

    récupération des verbes avec première visualisation

--- EnglishBattle/Controllers/GameController.cs
+++ EnglishBattle/Controllers/GameController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DbConnect;
+using DbConnect.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +12,25 @@ namespace EnglishBattle.Controllers
     {
         public ActionResult Game()
         {
+            List<Verbe> verbes;
+            if (Session["verbes"] == null) {
+                verbes = new VerbeServices(new DbConnect.EnglishBattleEntities()).GetVerbes();
+            } else {
+                verbes = (List<Verbe>) Session["verbes"];
+            }
+
+            // Genere un chiffre aleatoire pour le verbe choisi
+            int index = new Random().Next(verbes.Count() - 1);
+            // verbeAEnvoye = verbes[idAleatoire]
+            Verbe choosenVerb = verbes[index];
+            // Supprimer le verbe choisi
+            verbes.Remove(choosenVerb);
+            Session["verbes"] = verbes;
+
+            ViewBag.infinitif = choosenVerb.baseVerbale;
+            ViewBag.preterit = choosenVerb.preterit;
+            ViewBag.participePasse = choosenVerb.participePasse;
+
             return View();
         }
     }
